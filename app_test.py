fc06f85fa267ce9bd5b44efe737810274ac9f806
FarazArshad1 <newsletters833@gmail.com>
2024-12-19 13:42:14 +0500

    Use pytest Test Outcomes

--- app_test.py
+++ app_test.py
@@ -1,4 +1,9 @@
 from app import addition
+import pytest
 
+@pytest.mark.xfail()
 def test_addition():
-    assert addition(3,2) == 6
+    assert addition(3,2) == 5
+
+def test_addition_with_negative_values():
+    assert addition(-3,-2) == -5
